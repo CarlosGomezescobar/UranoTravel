a05692e15c25ac7fccef113db4548de144ab8ddb
iUranoO <uranoitech@gmail.com>
2024-07-27 18:17:54 -0400

    fu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import { ThirdwebProvider } from "thirdweb/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "thirdweb SDK + Next starter",
+  title: "Urano travel",
   description:
-    "Starter template for using thirdweb SDK with Next.js App router",
+    "Travel Web3 ",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <ConnectButton
             client={client}
             appMetadata={{
-              name: "Example App",
+              name: "Urano Travel",
               url: "https://example.com",
             }}
           />
