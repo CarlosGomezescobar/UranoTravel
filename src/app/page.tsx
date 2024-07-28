59b8644f2cc146d007fdb4ed7d450be94013b0a0
iUranoO <uranoitech@gmail.com>
2024-07-28 00:56:31 -0400

    thirdweb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,22 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { ConnectButton, ThirdwebProvider } from "thirdweb/react";
-import { useAddress } from "@thirdweb-dev/react";
+import { ThirdwebProvider, useAddress } from "@thirdweb-dev/react";
+import { ConnectButton } from "thirdweb/react";
 
 import Header from "../app/components/Header";
 import ThirdwebResources from "../app/components/ThirdwebResources";
 import { client } from "./client";
 
-export default function Home() {
+function WrappedHomePage() {
+  return (
+    <ThirdwebProvider>
+      <HomePage />
+    </ThirdwebProvider>
+  );
+}
+
+function HomePage() {
   const router = useRouter();
   const address = useAddress();
 
@@ -21,22 +29,22 @@ export default function Home() {
   }, [address, router]);
 
   return (
-    <ThirdwebProvider>
-      <main className="p-4 pb-10 min-h-screen flex items-center justify-center container max-w-screen-lg mx-auto bg-gray-900 text-white">
-        <div className="py-20 w-full">
-          <Header />
-          <div className="flex justify-center mb-20">
-            <ConnectButton
-              client={client}
-              appMetadata={{
-                name: "Urano Travel",
-                url: "https://example.com",
-              }}
-            />
-          </div>
-          <ThirdwebResources />
+    <main className="p-4 pb-10 min-h-screen flex items-center justify-center container max-w-screen-lg mx-auto bg-gray-900 text-white">
+      <div className="py-20 w-full">
+        <Header />
+        <div className="flex justify-center mb-20">
+          <ConnectButton
+            client={client}
+            appMetadata={{
+              name: "Urano Travel",
+              url: "https://example.com",
+            }}
+          />
         </div>
-      </main>
-    </ThirdwebProvider>
+        <ThirdwebResources />
+      </div>
+    </main>
   );
 }
+
+export default WrappedHomePage;
